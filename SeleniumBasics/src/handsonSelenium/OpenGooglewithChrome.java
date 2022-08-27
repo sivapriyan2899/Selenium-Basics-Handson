fb874b9c1d5ab2233d49fcafc7a39e9c6d48a682
siva <sivapriyan2899@gmail.com>
2022-08-27 12:02:18 +0530

    updated External jar path

--- SeleniumBasics/src/handsonSelenium/OpenGooglewithChrome.java
+++ SeleniumBasics/src/handsonSelenium/OpenGooglewithChrome.java
@@ -1,10 +1,19 @@
 package handsonSelenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class OpenGooglewithChrome {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
-	}
+		System.setProperty("webdriver.chrome.driver", "C:\\selenium\\WebDrivers\\chromedriver_win32\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https:\\google.co.in");
+		//Thread.sleep(3000);
+		driver.quit();
+		}
 
 }
