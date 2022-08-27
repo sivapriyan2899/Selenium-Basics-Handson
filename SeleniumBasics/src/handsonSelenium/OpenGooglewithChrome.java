36f88be64933841874da4477aca3ce24b5e705ab
siva <sivapriyan2899@gmail.com>
2022-08-27 15:05:46 +0530

    maximized browser

--- SeleniumBasics/src/handsonSelenium/OpenGooglewithChrome.java
+++ SeleniumBasics/src/handsonSelenium/OpenGooglewithChrome.java
@@ -11,9 +11,11 @@ public class OpenGooglewithChrome {
 		System.setProperty("webdriver.chrome.driver", "C:\\selenium\\WebDrivers\\chromedriver_win32\\chromedriver.exe");
 		
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.get("https:\\google.co.in");
 		//Thread.sleep(3000);
-		driver.quit();
+		//driver.quit();
+		
 		}
 
 }
